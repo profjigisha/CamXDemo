0c621562d4b7028565379c007096ad1728a4a18a
jkp <jigishapatel28@gmail.com>
2021-02-10 19:09:59 -0500

    Added Pick Image From Gallery Feature

---
+++ app/src/main/java/com/jk/camxdemo/CamXActivity.java
@@ -0,0 +1,14 @@
+package com.jk.camxdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CamXActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cam_x);
+    }
+}
--- app/src/main/java/com/jk/camxdemo/MainActivity.java
+++ app/src/main/java/com/jk/camxdemo/MainActivity.java
@@ -1,15 +1,19 @@
 package com.jk.camxdemo;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -58,10 +62,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             switch (index){
                                 case 0:
                                     Log.d(TAG, "Taking new picture");
-
+                                    openCamera();
                                     break;
                                 case 1:
                                     Log.d(TAG, "Getting Picture from Gallery");
+                                    selectFromGallery();
                                     break;
                                 case 2:
                                     dialogInterface.dismiss();
@@ -78,6 +83,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+    private void selectFromGallery(){
+        Intent pickPhotoIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        startActivityForResult(pickPhotoIntent, GALLERY_PICTURE_REQUEST_CODE);
+    }
+
+    private void openCamera(){
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK && requestCode == GALLERY_PICTURE_REQUEST_CODE && data != null){
+            this.imgPicture.setImageURI(data.getData());
+        }
+    }
 
     private void checkPermissions(){
         if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) &&
